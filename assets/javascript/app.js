dfd96b93c5e9906635b0d4abaef917eda3096b06
Level126Noob <38739330+Level126Noob@users.noreply.github.com>
2019-06-09 17:31:39 -0400

    Started over using constructor functions to cycle through questions better, rather than prompts.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,92 +1,63 @@
-
-//creating a constructor function
-function Question(text, choice, answer) {
+//creating a constructor function to define the questions
+function Question(text, choices, answer) {
     this.text = text;
-    this.choice = choice;
+    this.choices = choices;
     this.answer = answer;
 }
 
-
-//creating a function prototype to see if the user interface matches the answer.
-Question.prototype.correctAnswer = function(choice) {
+//creating a function to see if the user choice is equal to the answer (button click is user choice)
+Question.prototype.correctAnswer = function (choice) {
     return choice === this.answer;
 }
 
-//constructor function defining variables
+//========================creating a constructor function defining the games operations ========================================================================================================================
 function Quiz(questions) {
     this.score = 0;
     this.questions = questions;
     this.questionIndex = 0;
 }
 
-//writing a function to get the index of the current question and a guess
-Quiz.prototype.getQuestionIndex = function() {
+//creating a prototype function to recieve the correct question
+Quiz.prototype.getQuestionIndex = function () {
     return this.questions[this.questionIndex];
 }
 
-// function to check if the quiz has ended
-Quiz.prototype.isEnded = function() {
+//creating a prototype function that determines if the game is over
+Quiz.prototype.isEnded = function () {
     return this.questions.length === this.questionIndex;
 }
 
-//function to check if the current answer is equal to the answer that is selected by the user
-Quiz.prototype.guess = function(answer) {
+//creating a constructor prototype
+Quiz.prototype.guess = function (answer) {
     this.questionIndex++;
-    if(this.getQuestionIndex().correctAnswer(answer)) {
+
+    if (this.getQuestionIndex().correctAnswer(answer)) {
         this.score++;
     }
 }
+//======================================================== writing functions to work with the app interface====================================================================
 
-//a function to populate the question
+//making a function to populate the page with the questions
 function populate() {
     if(quiz.isEnded()) {
         //showScores();
     }
     else {
         //show question
-        var element = document.getElementById('question')
+        var element = document.getElementById("question");
         element.innerHTML = quiz.getQuestionIndex().text;
     }
 }
+//creating a questions array variable that matches the constructor function layout
+var questions = [
+    new Question("What year did the Cleveland Cavaliers win the NBA finals?", ["2015", "2016", "2017", "2018"], "2016"),
+    new Question("What year did the American Civil War End?", ["1852", "1878", "1843", "1865"], "1865"),
+    new Question("Is Puerto Rico is a part of the United States?", ["true", "false"], "false"),
+    new Question("What is the element for Iron?", ["Fe", "Se", "F", "De"], "Fe"),
+    new Question("How many hours are in a year?", ["365", "1870", "8760", "8500"], "8760")
+];
 
-var questions = [{
-        question: "What year did the Cleveland Cavaliers win the NBA finals?\n (a) 2015\n\ (b) 2016\n\ (c) 2017 \n\ (d) 2018",
-
-        answer: "b"
-    },
-    {
-        question: "What year did the American Civil War End?\n (a) 1852\n\ (b) 1878\n\ (c) 1843\n\ (d)1865",
-
-        answer: "d"
-    },
-    {
-        question: "Puerto Rico is a part of the United States?\n (a) true\n\ (b) false",
-
-        question: "b"
-    },
-    {
-        question: "What is the element for Iron?\n (a)Fe\n\ (b)Se\n\ (c)F\n\ (d)De",
-
-        answer: "a"
-    },
-    {
-        question: "How many hours are in a year?\n (a) 365\n\ (b) 1870\n\ (c) 8760\n\ (d) 8500",
-
-        answer: "d"
-    }
-]
-var quiz = Quiz(questions[i]);
+//creating a object for the Quiz constructor function
+var quiz = new Quiz(questions);
 
 populate();
-var score = 0;
-
-for (var i = 0; i < questions.length; i++) {
-    var response = window.prompt(questions[i].prompt)
-    if (response == questions[i].answer) {
-        score++
-        alert("Correct")
-    } else {
-        alert("Incorrect!")
-    }
-}
-alert("you got " + score + "/" + questions.length);
